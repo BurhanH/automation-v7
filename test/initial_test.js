f364a7df08efd67167463c2d6d451e9c3e3f1107
BurhanH <urazalinov@gmail.com>
2018-10-19 19:50:52 -0500

    Fixed by Codacy compliance

--- test/initial_test.js
+++ test/initial_test.js
@@ -1,15 +1,15 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('Test suite', function () {
+describe("Test suite", function () {
 
-    describe('Test One', function () {
-        it('should assert something', function () {
+    describe("Test One", function () {
+        it("should assert something", function () {
             assert(1 === 1);
         });
     });
 
-    describe('Test Two', function () {
-        it('should assert something else', function () {
+    describe("Test Two", function () {
+        it("should assert something else", function () {
             assert(10 > 9);
         });
     });
