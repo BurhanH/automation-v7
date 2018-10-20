96195422c56adae69b461fe16100a9db48a7520f
BurhanH <urazalinov@gmail.com>
2018-10-19 19:54:21 -0500

    Fixed by Codacy compliance 2

--- test/browser_test.js
+++ test/browser_test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const {Builder} = require('selenium-webdriver');
+const assert = require("assert");
+const {Builder} = require("selenium-webdriver");
 let driver;
 const TIMEOUT = 10000; // milliseconds
 
 function getDriver() {
-    driver = new Builder().forBrowser('firefox').build();
+    driver = new Builder().forBrowser("firefox").build();
     driver.manage().setTimeouts({implicit: TIMEOUT});
     driver.manage().window().setRect({width: 1280, height: 1024});
 
@@ -16,17 +16,17 @@ function closeDriver() {
     }
 }
 
-describe('Browser resolution', function () {
+describe("Browser resolution", function () {
    this.timeout(0); // Turning off mocha timeout for whole suite
-   describe('test resolution 800x600', function () {
-       it('resolution is set', async function () {
-           let set_width = 800;
-           let set_height = 600;
+   describe("test resolution 800x600", function () {
+       it("resolution is set", async function () {
+           let setWidth = 800;
+           let setHeight = 600;
            await getDriver();
-           await driver.manage().window().setRect({width: set_width, height: set_height});
+           await driver.manage().window().setRect({width: setWidth, height: setHeight});
            let {width, height} = await driver.manage().window().getRect();
-           assert(width === set_width, "Expected width " + set_width);
-           assert(height === set_height, "Expected height " + set_height);
+           assert(width === setWidth, "Expected width " + setWidth);
+           assert(height === setHeight, "Expected height " + setHeight);
            await closeDriver();
        });
     });
